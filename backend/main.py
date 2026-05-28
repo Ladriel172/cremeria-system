77a514d33560448f2283681671237720baa1e5cb
Adriel Garcia <garciaadriel172@gmail.com>
2026-05-28 11:45:29 -0600

    Fix GitHub Pages CORS

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,7 @@ ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
+    "https://ladriel172.github.io",
 ]
 
 # Agregar dominio de GitHub Pages si existe en env
