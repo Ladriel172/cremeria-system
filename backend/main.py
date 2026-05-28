b4112c41bd0c262180df093563bc3c5f8a6f372b
Adriel Garcia <garciaadriel172@gmail.com>
2026-05-27 23:03:34 -0600

    Fix render uvicorn

--- backend/main.py
+++ backend/main.py
@@ -101,8 +101,7 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8000))
     uvicorn.run(
-        app,
+        "main:app",
         host="0.0.0.0",
-        port=port,
-        reload=os.getenv("ENVIRONMENT", "development") == "development"
+        port=port
     )
